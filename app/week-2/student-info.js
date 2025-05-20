c093834e3c4197e4c6abfb0963acb5136cae658f
ChenLI0429 <Airchairin@gmail.com>
2025-05-20 10:36:51 -0600

    assignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+"use client";
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return(
+        <main>
+            <h1>Chen Li</h1>
+            <p className="bg-amber-200">
+                <Link href="https://github.com/ChenLiSait/cprg306-assignments">Github Repository: https://github.com/ChenLiSait/cprg306-assignments</Link>
+            </p>
+        </main>
+    )
+    
+}
